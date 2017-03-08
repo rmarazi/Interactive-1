752d515c8b9a4f356b7b0d3edf787efc09c6b2ee
rmarazi <rgopal@cca.edu>
2017-03-07 16:13:59 -0800

    d
    d

--- Projects/Project-02/Lecture-09-Intro-p5-Example/Example-06-Array/Page1/sketch.js
+++ Projects/Project-02/Lecture-09-Intro-p5-Example/Example-06-Array/Page1/sketch.js
@@ -13,18 +13,7 @@ function draw() {
 	textAlign(CENTER);
 	textSize(100);
 	fill(255,255,255);
-	var myLink;
-	text(name, width/4, height/2)
-	myLink = createA( 'Page1/page1.html','text link');
-
-		fill(255,0,0);
-	rect(width/2,0,width/2, height);
-	var names2=["2 sets of 3", "5 sets of 2", "4 sets of 20", "3 sets of 5", "4 sets of 15", "3 sets of 10"];
-	var name2 = random(names2);
-	textAlign(CENTER);
-	textSize(100);
-	fill(255,255,255);
-	text(name2, 3*width/4, height/1.9);
+	text(name, width/4, height/1.9);
 
 
 }
@@ -32,6 +21,21 @@ function draw() {
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
 }
+function draw() {
+	noLoop();
+	background(0,0,255);
+	fill(255,0,0);
+	rect(0,0,width/2, height);
+	var names=["2 sets of 3", "5 sets of 2", "dsdf", "sdss", "fdsdf", "sdfd", "Bacsdfk", "sdf", "Hamstrings", "Abs", "Shoulders", "Obliques"];
+	var name = random(names);
+	textAlign(CENTER);
+	textSize(100);
+	fill(255,255,255);
+	text(name, width/1.3, height/1.9);
 
 
+}
 
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+}
