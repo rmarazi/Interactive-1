3b182c881465d336187ca99b61575a9f5c51f184
rmarazi <rgopal@cca.edu>
2017-04-30 10:02:09 -0700

    pdf
    pdf

---
+++ Exercises/Exercise-11/site/accounts/rgopal.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: rgopal
+password: >
+  $2a$10$4c.3.apylmQ7q.td3V.G.uZTvKbtc2fU9oTfk5Fgfhlqt3k/VM3gC
+email: rgopal@cca.edu
+language: en
+role: admin
